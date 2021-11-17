6325a08082067517592457863bd32e448d81bcb7
Camryn Roadley <camryn.roadley@telus.com>
2021-11-17 14:17:27 -0800

    feat: additional changes

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
-
-var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
+var express = require('express'); // import the library
+var indexRouter = require('./routes/index'); // import the router module
 var cors = require('cors');
 
-var app = express();
+var app = express(); // create the express object
 
+// server configuration
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/todos', indexRouter);
+app.use('/todos', indexRouter); // load the router module
 
 module.exports = app;
